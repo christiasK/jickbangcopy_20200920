f3b0de8f4ee191420a65b06d59d52bbdc139de70
christias <christias@naver.com>
2020-09-19 16:21:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jickbangcopy_20200920/BaseActvity.kt
@@ -0,0 +1,11 @@
+package com.example.jickbangcopy_20200920
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    abstract fun setupEvents()
+
+    abstract fun setValues()
+
+}
